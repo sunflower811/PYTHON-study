28b68ce948356ee4761b1cfeda3443b4917ff57c
褚惠莙 <sunnychu811@gmail.com>
2025-02-07 03:42:05 +0000

    lesson8_10

--- LESSONS/lesson8/lesson8_1.py
+++ LESSONS/lesson8/lesson8_1.py
@@ -5,7 +5,18 @@ def get_names(nums:int=2)->list[str]:
     names:list[str]=names_str.split(sep='\n')
     names=random.choices(names,k=nums)
     return names
+def generate_students(names:list[str])->list[dict]:
+    students:list[dict]=[]
+    for name in names:
+        #print(name)
+        chinese=random.randint(50,100)
+        English=random.randint(50,100)
+        Math=random.randint(50,100)
+        student={'name':name,'chinese':chinese,'english':English,'math':Math}
+        students.append(student)
+    return students
+
 nums=int(input("students num(max=10)"))
 students_names:list[str]=get_names(nums=nums)
-for name in students_names:
-    print(name)
+students=generate_students(names=students_names)
+print(students)
