6b495510e67c5506f3d6bed37da28f5961a9a55a
褚惠莙 <sunnychu811@gmail.com>
2025-02-07 12:03:19 +0000

    lesson9_05

--- LESSONS/lesson9/lesson9_1.py
+++ LESSONS/lesson9/lesson9_1.py
@@ -1,9 +1,10 @@
-from gear import widget
+from gear.widget import name_G,G_state
+#import tools
 
 if __name__ =='__main__':
     n=int(input("Gine me a num(<10): "))
-    names=widget.name_G(n)
-    State:list[dict]=widget.G_state(names)
+    names=name_G(n)
+    State:list[dict]=G_state(names)
     for people in State:
         for key,value in people.items():
             print(f'{key}:{value}')
